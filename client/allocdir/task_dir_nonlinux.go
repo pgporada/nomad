846368205a986431345e6f74712404488ad6db85
Michael Schurter <schmichael@hashicorp.com>
2016-12-14 12:08:04 -0800

    Consolidate non-linux special dir handling

--- client/allocdir/task_dir_nonlinux.go
+++ client/allocdir/task_dir_nonlinux.go
@@ -1,11 +1,13 @@
+// +build !linux
+
 package allocdir
 
-// currently a noop on darwin
+// currently a noop on non-Linux platforms
 func (d *TaskDir) mountSpecialDirs() error {
 	return nil
 }
 
-// currently a noop on darwin
+// currently a noop on non-Linux platforms
 func (d *TaskDir) unmountSpecialDirs() error {
 	return nil
 }
